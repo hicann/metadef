bbbd760018e48e4f86eba54de6da615a2d58001e
likun104 <likun104@h-partners.com>
2026-01-14 14:28:48 +0800

    【PR】: update for mmpa headers change location
    Co-authored-by: likun104<likun104@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !10 merge br_update_for_mmpa_headers_change_location into master
    
    【PR】: update for mmpa headers change location
    
    Created-by: lihuachao
    Commit-by: likun104
    Merged-by: cann-robot
    Description: 
    
    # Pull Request
    
    ## 描述
    mmpa头文件在安装目录下从install−path/include目录迁移到{install-path}/include目录迁移到install−path/include目录迁移到{install-path}/pkg_inc目录，本仓依赖mmpa头文件路径需要做相应适配
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新功能
    - [ ] 💄 代码风格更新（格式化，局部变量）
    - [x] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [ ] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    
    
    ## 如何测试
    NA
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [x] 我的代码遵循了项目的代码风格
    - [x] 我已对代码进行了自测
    - [x] 我已更新了相关的文档
    - [x] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [x] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    
    
    
    See merge request: cann/metadef!10

--- cmake/modules/sub_module/Findmmpa.cmake
+++ cmake/modules/sub_module/Findmmpa.cmake
@@ -44,8 +44,9 @@ unset(_cmake_targets_defined)
 unset(_cmake_targets_not_defined)
 unset(_cmake_expected_targets)
 
-find_path(_INCLUDE_DIR
-    NAMES include/mmpa/mmpa_api.h
+find_path(mmpa_INCLUDE_DIR
+    NAMES mmpa/mmpa_api.h
+    PATH_SUFFIXES pkg_inc
     NO_CMAKE_SYSTEM_PATH
     NO_CMAKE_FIND_ROOT_PATH)
 
@@ -66,13 +67,12 @@ find_package_handle_standard_args(mmpa
     FOUND_VAR
         mmpa_FOUND
     REQUIRED_VARS
-        _INCLUDE_DIR
+        mmpa_INCLUDE_DIR
         mmpa_SHARED_LIBRARY
         mmpa_STATIC_LIBRARY
 )
 
 if(mmpa_FOUND)
-    set(mmpa_INCLUDE_DIR "${_INCLUDE_DIR}/include")
     include(CMakePrintHelpers)
     message(STATUS "Variables in mmpa module:")
     cmake_print_variables(mmpa_INCLUDE_DIR)
@@ -109,4 +109,4 @@ if(mmpa_FOUND)
 endif()
 
 # Cleanup temporary variables.
-set(_INCLUDE_DIR)
+set(mmpa_INCLUDE_DIR)
