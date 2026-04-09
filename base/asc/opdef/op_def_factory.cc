34ffdfbf54f34ff2e3f420e383b37ed23b6c2dbe
huxj153 <huxiujuan@huawei.com>
2026-04-09 22:20:47 +0800

    [重构]: opdef解耦
    Co-authored-by: 我乌拉乌拉了<huxiujuan@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !195 merge f/b into develop
    
    [重构]: opdef解耦
    
    Created-by: huxj153
    Commit-by: 我乌拉乌拉了
    Merged-by: cann-robot
    Description: 
    
    # Pull Request
    
    ## 描述
    背景为opdef解耦，需要把ge里opdef的具体实现下沉到metadef里，本pr的目的是：
    1. 把ge的opdef的实现抽取到metadef里
    2. 并由metadef将opdef的头文件打入cann包
    3. metadef提供opdef具体实现的包间接口给ge调用
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新功能
    - [ ] 💄 代码风格更新（格式化，局部变量）
    - [x] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [ ] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    
    
    ## 如何测试
    描述测试此变更的步骤和前提条件：
    1.
    2.
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [x] 我的代码遵循了项目的代码风格
    - [x] 我已对代码进行了自测
    - [x] 我已更新了相关的文档
    - [x] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [x] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    
    
    
    See merge request: cann/metadef!195

---
+++ base/asc/opdef/op_def_factory.cc
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of 
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <set>
+#include "register/op_def.h"
+#include "base/asc/opdef/op_def_impl.h"
+#include "register/op_def_factory.h"
+#include "base/asc/opdef/op_def_factory_impl.h"
+
+namespace ops {
+
+int OpDefFactory::OpDefRegister(const char *name, OpDefCreator creator) {
+  return OpDefFactoryImpl::GetInstance().OpDefRegister(name, creator);
+}
+
+int __attribute__((weak)) OpDefFactory::OpDefRegisterV2(const char *name, OpDefFuncPtr ptr) {
+  return OpDefFactoryImpl::GetInstance().OpDefRegisterV2(name, ptr);
+}
+
+OpDef OpDefFactory::OpDefCreate(const char *name) {
+  return OpDefFactoryImpl::GetInstance().OpDefCreate(name);
+}
+
+std::vector<ge::AscendString> &OpDefFactory::GetAllOp(void) {
+  return OpDefFactoryImpl::GetInstance().GetAllOp();
+}
+
+void OpDefFactory::OpTilingSinkRegister(const char *opType) {
+  OpDefFactoryImpl::GetInstance().OpTilingSinkRegister(opType);
+}
+
+bool OpDefFactory::OpIsTilingSink(const char *opType) {
+  return OpDefFactoryImpl::GetInstance().OpIsTilingSink(opType);
+}
+}  // namespace ops
