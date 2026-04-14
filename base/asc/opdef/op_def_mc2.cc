33972445ddb5b9e42d4b768083501e13bd91dc2a
huxj153 <huxiujuan@huawei.com>
2026-04-14 20:51:14 +0800

    [重构]: opdef解耦
    Co-authored-by: 我乌拉乌拉了<huxiujuan@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !232 merge f/b7 into develop
    
    [重构]: opdef解耦
    
    Created-by: huxj153
    Commit-by: 我乌拉乌拉了
    Merged-by: cann-robot
    Description: 
    
    # Pull Request
    
    ## 描述
    背景为opdef解耦，需要把ge里opdef的具体实现下沉到metadef里，本pr的目的是：
    1. 把ge的opdef的实现抽取到metadef里
    2. 并由metadef将opdef的头文件打入cann包
    3. metadef提供opdef具体实现的包间接口给ge调用
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新功能
    - [ ] 💄 代码风格更新（格式化，局部变量）
    - [x] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [ ] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    
    
    ## 如何测试
    描述测试此变更的步骤和前提条件：
    1.
    2.
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [x] 我的代码遵循了项目的代码风格
    - [x] 我已对代码进行了自测
    - [x] 我已更新了相关的文档
    - [x] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [x] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    
    
    
    See merge request: cann/metadef!232

---
+++ base/asc/opdef/op_def_mc2.cc
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of 
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include <algorithm>
+#include "base/asc/opdef/op_def_impl.h"
+
+namespace ops {
+OpMC2Def::OpMC2Def() : impl_(new(std::nothrow) OpMC2DefImpl) {}
+
+OpMC2Def::OpMC2Def(const OpMC2Def &mc2_def) : impl_(new(std::nothrow) OpMC2DefImpl) {
+  this->impl_->Construct(this, mc2_def);
+}
+
+OpMC2Def::~OpMC2Def() = default;
+
+OpMC2Def &OpMC2Def::operator=(const OpMC2Def &mc2_def) {
+  return this->impl_->Eq(this, mc2_def);
+}
+
+OpMC2Def &OpMC2Def::HcclGroup(const char *value) {
+  return this->impl_->HcclGroup(this, value);
+}
+
+OpMC2Def &OpMC2Def::HcclGroup(std::vector<const char *> value) {
+  return this->impl_->HcclGroup(this, value);
+}
+
+std::vector<ge::AscendString> &OpMC2Def::GetHcclGroups(void) const {
+  return this->impl_->group_list;
+}
+
+void OpMC2Def::HcclServerType(enum HcclServerType type, const char* soc) {
+  this->impl_->HcclServerTypeImpl(this, type, soc);
+}
+
+/**
+ * @brief get hccl server type by soc version
+ * @param soc_version "" means checking if any hccl server type has been set
+ * @return hccl server type corresponding to soc version.
+           For scenarios where soc version is empty, return MAX if not set, AICPU if set.
+ */
+enum HcclServerType OpMC2Def::GetHcclServerType(const ge::AscendString &soc_version) const {
+  return this->impl_->GetHcclServerType(this, soc_version);
+}
+
+}  // namespace ops
