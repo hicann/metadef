4dc15477118ce6a6fefbe0f0af5de0c3801ad25d
jikai-tyler <jikai5@huawei.com>
2026-06-25 21:38:26 +0800

    【PR】: 对外头文件整改修改配置文件
    Co-authored-by: jikai-tyler<jikai5@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !378 merge metadef_external_910 into 9.1.0
    
    【PR】: 对外头文件整改修改配置文件
    
    Created-by: jikai-tyler
    Commit-by: jikai-tyler
    Merged-by: cann-robot
    Description: 
    
    # Pull Request
    
    ## 描述
    对外头文件整改，之前修改合入了新增头文件，本次修改配置文件，将新增头文件打入包中。
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [x] ✨ 新功能
    - [ ] 💄 代码风格更新（格式化，局部变量）
    - [ ] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [ ] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    
    
    ## 如何测试
    描述测试此变更的步骤和前提条件：
    1.
    2.
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [x] 我的代码遵循了项目的代码风格
    - [x] 我已对代码进行了自测
    - [x] 我已更新了相关的文档
    - [x] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [x] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    
    
    
    See merge request: cann/metadef!378

--- cmake/package.cmake
+++ cmake/package.cmake
@@ -128,6 +128,7 @@ set(EXTERNAL_RUNTIME_FILES
     ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/exe_graph/runtime/storage_format.h
     ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/exe_graph/runtime/stride.h
     ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/exe_graph/runtime/tensor.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/exe_graph/runtime/runtime_tensor.h
     ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/exe_graph/runtime/continuous_vector.h
     ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/exe_graph/runtime/exe_res_generation_context.h
     ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/exe_graph/runtime/infer_shape_context.h
@@ -153,6 +154,7 @@ set(EXTERNAL_GRAPH_FILES
     ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/graph/tensor.h
     ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/graph/c_types.h
     ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/graph/ge_error_codes.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/graph/error_codes.h
     ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/graph/infer_format_context.h
     ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/graph/compiler_def.h
     ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/graph/ascend_string.h
@@ -176,6 +178,7 @@ install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/ge/ge_allocator.h
 )
 install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/ge_common/ge_api_types.h
               ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/ge_common/ge_api_error_codes.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/ge_common/api_error_codes.h
               ${CMAKE_CURRENT_SOURCE_DIR}/inc/external/ge_common/ge_error_codes.h
     DESTINATION ${CMAKE_SYSTEM_PROCESSOR}-linux/include/external/ge_common
     COMPONENT metadef
