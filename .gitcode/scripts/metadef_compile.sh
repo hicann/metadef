d40057757913917ed24c9e6b7c22dd4548765f02
wangyunxiang <wangyunxiang6@h-partners.com>
2026-09-02 14:44:57 +0800

    update actions 适配gcc15

--- .gitcode/scripts/metadef_compile.sh
+++ .gitcode/scripts/metadef_compile.sh
@@ -21,12 +21,11 @@ function LOG_DO() {
 }
 
 cd "${WORKSPACE}" || exit 1
-grep -E "^VERSION_ID=" /etc/os-release | cut -d'"' -f2
-
 if [[ "${task_name}" == *ubuntu24* ]]; then
-    sudo update-alternatives --set gcc /usr/bin/gcc-14
-    if ! head -1 "CMakeLists.txt" | grep -q "CMAKE_EXPORT_COMPILE_COMMANDS"; then
-        sed -i "1i set(CMAKE_EXPORT_COMPILE_COMMANDS ON)" "CMakeLists.txt"
+    if [ "${GIT_TARGET_BRANCH}" == "master" ]; then
+        sudo update-alternatives --set gcc /usr/bin/gcc-15
+    else
+        sudo update-alternatives --set gcc /usr/bin/gcc-14
     fi
 else
     if [[ -f "/opt/rh/devtoolset-7/enable" ]]; then
@@ -34,11 +33,28 @@ else
         source /opt/rh/devtoolset-7/enable
     fi
 fi
-
+if gcc --version | head -n1 | grep -q "15\."; then
+    rm -rf /home/jenkins/opensource/lib_cache
+    if [ -d /home/jenkins/opensource/gcc15 ]; then
+        rm -rf /home/jenkins/opensource/gcc15/lib_cache/abseil-cpp
+        rm -rf /home/jenkins/opensource/gcc15/lib_cache/device/abseil-cpp
+        ln -s /home/jenkins/opensource/gcc15/lib_cache/ /home/jenkins/opensource/lib_cache
+    elif [ -d /home/jenkins/opensource/gcc15x86 ]; then
+        rm -rf /home/jenkins/opensource/gcc15x86/lib_cache/abseil-cpp
+        rm -rf /home/jenkins/opensource/gcc15x86/lib_cache/device/abseil-cpp
+        ln -s /home/jenkins/opensource/gcc15x86/lib_cache/ /home/jenkins/opensource/lib_cache
+    fi
+elif gcc --version | head -n1 | grep -q "14\."; then
+    gcc --version
+else
+    gcc --version
+    rm -rf /home/jenkins/opensource/lib_cache
+    ln -s /home/jenkins/opensource/ubuntu20/lib_cache /home/jenkins/opensource/lib_cache
+fi
 gcc --version
 source /home/jenkins/Ascend/cann/bin/setenv.bash
 
-if [[ "${task_name}" =~ Compile_Asend_ARM_ubuntu24 ]]; then
+if [[ "${task_name}" =~ Compile_Asend_X86_ubuntu24 ]]; then
     echo "api-check=compile" >> "${ATOMGIT_OUTPUT}"
 else
     echo "api-check=continue" >> "${ATOMGIT_OUTPUT}"
