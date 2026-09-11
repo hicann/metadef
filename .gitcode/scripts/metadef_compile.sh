ff9cceaa09f2bef45d1361d3085546929dc9fbbe
wangyunxiang <wangyunxiang6@h-partners.com>
2026-09-11 11:28:27 +0800

    update gcc切换方式

--- .gitcode/scripts/metadef_compile.sh
+++ .gitcode/scripts/metadef_compile.sh
@@ -22,10 +22,12 @@ function LOG_DO() {
 
 cd "${WORKSPACE}" || exit 1
 if [[ "${task_name}" == *ubuntu24* ]]; then
-    if [ "${GIT_TARGET_BRANCH}" == "master" ]; then
-        sudo update-alternatives --set gcc /usr/bin/gcc-15
-    else
-        sudo update-alternatives --set gcc /usr/bin/gcc-14
+    if sudo update-alternatives --set gcc /usr/bin/gcc-16 2>/dev/null; then
+        echo "Switched to gcc-16"
+    elif sudo update-alternatives --set gcc /usr/bin/gcc-15 2>/dev/null; then
+        echo "Switched to gcc-15"
+    elif sudo update-alternatives --set gcc /usr/bin/gcc-14 2>/dev/null; then
+        echo "gcc-16/15 not available, fell back to gcc-14"
     fi
 else
     if [[ -f "/opt/rh/devtoolset-7/enable" ]]; then
